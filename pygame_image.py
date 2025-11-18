2da5f4a2047c6a620498380a3dbf68637a17fccb
suda <c0c240350f@edu.teu.ac.jp>
2025-11-18 16:45:50 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,7 @@ def main():
 
         x=tmr
         screen.blit(bg_img, [-x, 0])#練習５
+        screen.blit(bg_img, [-x+1600, 0])
         screen.blit(kk_img,[300,200])#練習４
         pg.display.update()
         tmr += 1        
