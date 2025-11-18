52cc05c2f9dcfe0a3ba4aed4e3d5cfee108bb9c9
suda <c0c240350f@edu.teu.ac.jp>
2025-11-18 18:00:16 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -34,6 +34,8 @@ def main():
             kk_rect.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
             kk_rect.move_ip((+1,0))
+        else:
+            kk_rect.move_ip((-1,0))
         screen.blit(kk_img,kk_rect)#練習１０
         pg.display.update()
         tmr += 1        
