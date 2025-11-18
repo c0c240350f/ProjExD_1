bd3490ea7d6e2ccfa12d4b6f5e6834c74df94e1c
suda <c0c240350f@edu.teu.ac.jp>
2025-11-18 17:10:53 +0900

    練習9

--- pygame_image.py
+++ pygame_image.py
@@ -18,9 +18,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x=tmr
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0])#練習５
         screen.blit(bg_img2, [-x+1600, 0])#練習8
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img,[300,200])#練習４
         pg.display.update()
         tmr += 1        
