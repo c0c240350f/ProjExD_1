5b10724bf9bebfad340f8f0eb3e0e0a21a9a303b
suda <c0c240350f@edu.teu.ac.jp>
2025-11-18 17:24:48 +0900

    練習10

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,28 @@ def main():
     bg_img2=pg.transform.flip(bg_img,True,False)
     kk_img=pg.image.load("fig/3.png")
     kk_img=pg.transform.flip(kk_img,True,False)#練習３
+    kk_rect=kk_img.get_rect()
+    kk_rect.center=300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x=tmr%3200
+        x=tmr%3200#練習９
         screen.blit(bg_img, [-x, 0])#練習５
         screen.blit(bg_img2, [-x+1600, 0])#練習8
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img,[300,200])#練習４
+
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rect.move_ip((0,+1))
+        if key_lst[pg.K_LEFT]:
+            kk_rect.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip((+1,0))
+        screen.blit(kk_img,kk_rect)#練習１０
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習６
